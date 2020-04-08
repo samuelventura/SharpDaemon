8474591f68915a2a22974882d555708280a7d1a0
Samuel Ventura <samuel.ventura@yeico.com>
2020-04-08 18:08:09 -0500

    Added installer.bat to create installer with inno setup 5.

--- Installer.bat
+++ Installer.bat
@@ -1,3 +1,6 @@
+set INNO=C:\Program Files (x86)\Inno Setup 5
+
+cd %~dp0
 dotnet publish Daemon.StaticWebServer -c Release
 dotnet publish SharpDaemon.Service -c Release
-
+"%INNO%\iscc.exe" Installer.iss
