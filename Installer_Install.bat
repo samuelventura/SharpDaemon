4a41201e8abcaeee6146fd1b6b77c896c6194392
Samuel Ventura <samuel.ventura@yeico.com>
2020-04-08 20:34:51 -0500

    Installer build, install, and uninstall bat files.

---
+++ Installer_Install.bat
@@ -0,0 +1,2 @@
+cd %~dp0
+Output\DaemonManager.Setup-1.0.1.exe /VERYSILENT /SUPPRESSMSGBOXES /NORESTART
---
+++ Installer_Uninstall.bat
@@ -0,0 +1,2 @@
+cd %~dp0
+"C:\Program Files (x86)\Samuel Ventura\Daemon Manager\unins000.exe" /VERYSILENT /SUPPRESSMSGBOXES /NORESTART
